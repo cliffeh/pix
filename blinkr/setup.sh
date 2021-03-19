9e119474ff1bbe87f244cc84aa2467a6a4299452
Cliff Snyder <cliff.snyder@gmail.com>
2021-03-19 16:48:33 -0400

    python3

--- blinkr/setup.sh
+++ blinkr/setup.sh
@@ -5,7 +5,7 @@ set -e
 BLINKR_HOME="$(dirname $0)"
 echo "Using BLINKR_HOME=${BLINKR_HOME}"
 
-python -m venv "${BLINKR_HOME}/venv"
+python3 -m venv "${BLINKR_HOME}/venv"
 source "${BLINKR_HOME}/venv/bin/activate"
 pip install -r "${BLINKR_HOME}/requirements.txt"
 

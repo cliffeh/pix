c9d58f9abb931d5bd8a905f16a249b0bd26785ac
Cliff Snyder <cliff.snyder@gmail.com>
2021-03-19 16:57:00 -0400

    0.0.0.0

--- blinkr/run.sh
+++ blinkr/run.sh
@@ -5,4 +5,5 @@ echo "Using BLINKR_HOME=${BLINKR_HOME}"
 
 source "${BLINKR_HOME}/venv/bin/activate"
 export FLASK_APP="${BLINKR_HOME}/blinkr.py"
-flask run --reload --extra-files templates/index.html
+# TODO make 0.0.0.0 optional
+flask run --host 0.0.0.0 --reload --extra-files templates/index.html

32b98f18bdeeb2a6cfdf3dea5030062b5dce7d7d
Cliff Snyder <cliff.snyder@gmail.com>
2021-03-19 14:33:42 -0400

    make it actually work

--- blinkr/blinkr.py
+++ blinkr/blinkr.py
@@ -1,7 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('hello.html', name='blink')
-
+    return render_template('index.html', name='blink')

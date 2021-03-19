9f55491e63fd375269547acac7f0f71298bee75a
Cliff Snyder <cliff.snyder@gmail.com>
2021-03-19 14:42:16 -0400

    super-basic run script

---
+++ blinkr/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+BLINKR_HOME="$(dirname $0)"
+echo "Using BLINKR_HOME=${BLINKR_HOME}"
+
+source "${BLINKR_HOME}/venv/bin/activate"
+export FLASK_APP="${BLINKR_HOME}/blinkr.py"
+flask run
